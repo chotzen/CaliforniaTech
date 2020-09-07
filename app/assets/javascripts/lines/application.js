527ecee1db30a5fa63d8a002982a9f0af209aaf8
Devin Hartzell <devin@caltech.edu>
2020-09-06 20:34:50 -1000

    Fix myriad footer/typography issues

--- app/assets/javascripts/lines/application.js
+++ app/assets/javascripts/lines/application.js
@@ -10,10 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require lines/viewer
-//= require lines/admin/navbar
-//= require popper
-//= require bootstrap
+
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( lines/admin/admin.js lines/admin/admin.css )
+Rails.application.config.assets.precompile += %w( application.js lines/admin/admin.js lines/admin/admin.css )
 Rails.application.config.assets.precompile += %w( favicon.ico ic_rss.png )
